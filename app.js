292c02c1ad12b25df1931d1b68aeeecbcc91c52b
NnatasaK <natasakolaric2@gmail.com>
2023-12-13 11:09:05 +0100

    git repository initialized

---
+++ app.js
@@ -0,0 +1,15 @@
+const express = require('express');
+const app = express()
+
+
+//routes
+
+app.get('/', (req, res) => {
+    res.send('hiiii node api');
+})
+
+
+app.listen(3000, () => {
+    console.log('App running on port 3000')
+})
+
